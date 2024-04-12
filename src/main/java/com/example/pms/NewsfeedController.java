f5be925db03b1faf9a2d661da899a3e8ff1a14fe
Tayrin Tunzina <ttunzina202145@bscse.uiu.ac.bd>
2024-04-13 03:25:41 +0600

    send & view project css added

--- src/main/java/com/example/pms/NewsfeedController.java
+++ src/main/java/com/example/pms/NewsfeedController.java
@@ -311,6 +311,24 @@ public class NewsfeedController implements Initializable {
     }
 
     @FXML
+    void sendProject(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("sendProject.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    void getProject(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("getProject.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
     void logout(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
